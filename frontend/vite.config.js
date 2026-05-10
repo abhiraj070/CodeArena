3ffe4b897d9d6941c4c5e9458602f45935feb721
Abhiraj Sharma <iamabhirajsharma@gmail.com>
2026-05-10 21:11:50 +0530

    vite.config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,41 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	server: {
-		proxy: {
-			"/feature": {
-                target: import.meta.env.VITE_STATE==="production" ? "https://api-gateway-kls3.onrender.com" : "http://localhost:8003", 
-                changeOrigin: true,
-            },
-            "/codeRun": {
-                target: import.meta.env.VITE_STATE==="production" ? "https://api-gateway-kls3.onrender.com" : "http://localhost:8003",
-                changeOrigin: true,
-            }
+export default defineConfig(({ mode }) => {
+
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react(), tailwindcss()],
+
+		server: {
+			proxy: {
+				"/feature": {
+					target:
+						env.VITE_STATE === "production"
+							? "https://api-gateway-kls3.onrender.com"
+							: "http://localhost:8003",
+
+					changeOrigin: true,
+				},
+
+				"/codeRun": {
+					target:
+						env.VITE_STATE === "production"
+							? "https://api-gateway-kls3.onrender.com"
+							: "http://localhost:8003",
+
+					changeOrigin: true,
+				},
+			},
 		},
-	},
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
 		},
-        
-	},
+	};
 });
